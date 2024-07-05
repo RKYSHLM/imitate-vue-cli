d8972d359b98033752704d71b63d5397974dbc27
shanquanquan <1149460755@qq.com>
2024-07-05 16:57:38 +0800

    force和create实现

---
+++ bin/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+// 1 配置可执行的命令 commander
+import { Command } from 'commander'
+import chalk from 'chalk'
+import config from '../package.json' assert { type: 'json' }
+
+const program = new Command()
+
+program
+	.command('create <app-name>') // 创建命令
+	.description('create a new project') // 命令描述
+	.option('-f, --force', 'overwrite target directory if it is existed') // 命令选项(选项名，描述) 这里就是解决下重名的情况
+	.action((name, options, cmd) => {
+		import('../lib/create.js').then(({ default: create }) => {
+			create(name, options, cmd)
+		})
+	})
+
+program.on('--help', () => {
+	console.log()
+	console.log(`Run ${chalk.cyan('rippi <command> --help')} to show detail of this command`)
+	console.log()
+})
+
+program
+	// 说明版本
+	.version(`rippi-cli@${config.version}`)
+	// 说明使用方式
+	.usage('<command> [options]')
+
+
+// 解析用户执行命令传入的参数
+program.parse(process.argv)
